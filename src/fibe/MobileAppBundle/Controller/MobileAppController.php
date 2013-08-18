bb89bdc1c1f90d58a0831cc350d270e6e2a0d21b
florian.bacle <florian.bacle@atos.net>
2013-08-18 02:49:37 +0200

    embed dataConf in mobile app section view

--- src/fibe/MobileAppBundle/Controller/MobileAppController.php
+++ src/fibe/MobileAppBundle/Controller/MobileAppController.php
@@ -6,10 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * Mobile app controller.
+ *
+ * @Route("/MobileApplication")
+ */
 class MobileAppController extends Controller
 {
     /**
-     * @Route("/MobileApplication") name="mobileApp_index"
+     * @Route("/",name="mobileApp_index")
      * @Template()
      */
     public function indexAction()
--- src/fibe/UserDataBundle/Controller/UserDataController.php
+++ src/fibe/UserDataBundle/Controller/UserDataController.php
@@ -6,10 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * user data controller.
+ *
+ * @Route("/UserData")
+ */
 class UserDataController extends Controller
 {
     /**
-     * @Route("/UserData") name="userData_index"
+     * @Route("/",name="userData_index")
      * @Template()
      */
     public function indexAction()
