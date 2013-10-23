d57a17e17f03c8f59082df3984be447b3d036b71
florian.bacle <bacle.florian@gmail.com>
2013-10-23 22:21:45 +0200

    route organization

---
+++ src/fibe/Bundle/WWWConfBundle/Form/OrganizationType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace fibe\Bundle\WWWConfBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class OrganizationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('page')
+            ->add('country')
+            ->add('members')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'fibe\Bundle\WWWConfBundle\Entity\Organization'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'fibe_bundle_wwwconfbundle_organizationtype';
+    }
+}
