466355652546b34312affdb598f552da979ca1f0
florian.bacle <bacle.florian@gmail.com>
2013-11-14 00:09:24 +0100

    ics management

--- vendor/idci/exporter-bundle/IDCI/Bundle/ExporterBundle/Export/IcsExport.php
+++ vendor/idci/exporter-bundle/IDCI/Bundle/ExporterBundle/Export/IcsExport.php
@@ -15,11 +15,11 @@ class IcsExport extends AbstractExport
     
     public function buildHeader()
     {
-        $this->setContent('BEGIN:VCALENDAR'.PHP_EOL.'VERSION:2.0'.PHP_EOL.'PRODID:-//hacksw/handcal//NONSGML v1.0//EN'.PHP_EOL.PHP_EOL);
+       // $this->setContent('BEGIN:VCALENDAR'.PHP_EOL.'VERSION:2.0'.PHP_EOL.'PRODID:-//hacksw/handcal//NONSGML v1.0//EN'.PHP_EOL.PHP_EOL);
     }
 
     public function buildFooter()
     {
-        $this->setContent($this->getContent().'END:VCALENDAR');
+      //  $this->setContent($this->getContent().'END:VCALENDAR');
     }
 }
