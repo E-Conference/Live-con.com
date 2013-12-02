2f71ac8f7f855bc097dc4c7336f769c7141dba69
BenoitDdlp <benoitddlp@msn.com>
2013-12-02 21:27:44 +0100

    Two days overlap for event in schedule view fix

--- src/fibe/Bundle/WWWConfBundle/Resources/public/js/fullcalendar/added/CalEvent.js
+++ src/fibe/Bundle/WWWConfBundle/Resources/public/js/fullcalendar/added/CalEvent.js
@@ -139,7 +139,7 @@ CalEvent.prototype.updateParentDate = function(){
           return;
         } 
  
-        if(event.isInsideOf(parent))return;  
+        // if(event.isInsideOf(parent))return;  
 
         //event is out of parent
         console.log("isOutOfParent");  
@@ -271,6 +271,21 @@ CalEvent.prototype.SetRecurDate = function(){
         child.persist();
       } 
 };
+CalEvent.prototype.fitToDay = function (oldStart,oldEnd){
+  var duration =  moment(oldEnd).diff(moment(oldStart));
+  var midnightLimit = moment(this.end).startOf("day").format(); 
+  if(moment(this.start).diff(midnightLimit) > moment(midnightLimit).diff(this.end)){
+  // if(this.start < oldStart){
+    //we put the event to the next day
+    this.start = moment(midnightLimit).format();
+    this.end = moment(this.start).add(duration).format(); 
+
+  }else{
+    //we put the event to the previous day
+    this.end = moment(midnightLimit).format();
+    this.start = moment(this.end).subtract(duration).format(); 
+  }
+}
 
 // remove relation with old parent if exists
 // and update relation with new parent (render event but dont persist changes to db)
@@ -397,6 +412,10 @@ CalEvent.prototype.isInsideOf = function(event){
 CalEvent.prototype.isInstant = function(){
     var diff =moment(this["start"]).diff(this["end"]);
     return (diff  === 0 ) || (diff  === 1 );   
+};
+
+CalEvent.prototype.isOneDayLong = function(){ 
+    return (moment(this["start"]).dayOfYear() == moment(this["end"]).dayOfYear()); 
 };
 
 CalEvent.prototype.formatDate = function () {  
