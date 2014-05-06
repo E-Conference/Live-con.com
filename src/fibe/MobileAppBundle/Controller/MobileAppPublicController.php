78c4fc36b1b588d06c36071829e7b4f9e36f11ab
BenoitDdlp <benoitddlp@msn.com>
2014-05-07 00:48:07 +0200

    api rest fix topic & co
    Conflicts:
    	src/fibe/MobileAppBundle/Controller/MobileAppPublicController.php

--- src/fibe/MobileAppBundle/Controller/MobileAppPublicController.php
+++ src/fibe/MobileAppBundle/Controller/MobileAppPublicController.php
@@ -60,23 +60,39 @@
     {
       $em = $this->getDoctrine()->getManager();
 
-      $conference = $em->getRepository('fibeWWWConfBundle:WwwConf')->findOneBySlug($slug);
-      if (!$conference)
-      {
-        throw new NotFoundHttpException();
-      }
-      $mobile_app_config = $conference->getAppConfig();
-      $baseUri = "http://data.live-con.com/resource/conference/" . $conference->getId() . "/" . $conference->getSlug();
-      $apiUri = "http://data.live-con.com/sparql";
-      $apiType = "sparql";
-      return array(
-        'api_uri'             => $apiUri,
-        'api_type'            => $apiType,
-        'paper_module'        => $conference->getModule()->getPaperModule(),
-        'organization_module' => $conference->getModule()->getOrganizationModule(),
-        'conference_baseUri'  => $baseUri,
-        'mobile_app_config'   => $mobile_app_config,
-        'conference'          => $conference,
-      );
+        $conference = $em->getRepository('fibeWWWConfBundle:WwwConf')->findOneBySlug($slug);
+        $mobile_app_config = $conference->getAppConfig();
+        $apiUri = $this->get('router')->generate('idci_exporter_api_homeapi');
+        $apiType = "rest";
+        $baseUri = "http://data.live-con.com/resource/conference/" . $conference->getId() . "/" . $conference->getSlug();
+        return array(
+            'api_uri' => $apiUri,
+            'api_type' => $apiType,
+            'paper_module' => $conference->getModule()->getPaperModule(),
+            'organization_module' => $conference->getModule()->getOrganizationModule(),
+            'conference_baseUri' => $baseUri,
+            'mobile_app_config' => $mobile_app_config,
+            'conference' => $conference,
+        );
+        /*
+        $em = $this->getDoctrine()->getManager();
+
+        $conference = $em->getRepository('fibeWWWConfBundle:WwwConf')->findOneBySlug($slug);
+        if(!$conference){
+            throw new NotFoundHttpException();
+        }
+        $mobile_app_config = $conference->getAppConfig();
+        $baseUri = "http://data.live-con.com/resource/conference/".$conference->getId()."/".$conference->getSlug();
+        $apiUri = "http://data.live-con.com/sparql";
+        $apiType = "sparql";
+        return array(
+            'api_uri' => $apiUri,
+            'api_type' => $apiType,
+            'paper_module' => $conference->getModule()->getPaperModule(),
+            'organization_module' => $conference->getModule()->getOrganizationModule(),
+            'conference_baseUri' => $baseUri,
+            'mobile_app_config' => $mobile_app_config,
+            'conference' => $conference,
+        );*/
     }
   }
