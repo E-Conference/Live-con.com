f1858f00ee0a2fbbd4ccdf309647e81b6ceb1fb3
Vincent Sébille <vincent.sebille@gmail.com>
2014-05-30 16:08:54 +0200

    [fix] Réparation init projet

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,47 +5,48 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-    public function registerBundles()
-    {
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
-            new IDCI\Bundle\ExporterBundle\IDCIExporterBundle(),
-            new fibe\Bundle\WWWConfBundle\fibeWWWConfBundle(),
-            new fibe\SecurityBundle\fibeSecurityBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
-            new fibe\MobileAppBundle\fibeMobileAppBundle(),
-            new fibe\UserDataBundle\fibeUserDataBundle(),
-            new fibe\DashboardBundle\fibeDashboardBundle(),
-            new fibe\HomePageBundle\fibeHomePageBundle(),
-            new fibe\DataBundle\DataBundle(),
-            new fibe\ExternalizationBundle\fibeExternalizationBundle(),
-            new fibe\ConferenceBundle\fibeConferenceBundle(),
-            new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
-            new fibe\DocumentationBundle\DocumentationBundle(),
-        );
+  public function registerBundles()
+  {
+    $bundles = array(
+      new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+      new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+      new Symfony\Bundle\TwigBundle\TwigBundle(),
+      new Symfony\Bundle\MonologBundle\MonologBundle(),
+      new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+      new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+      new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+      new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+      new JMS\AopBundle\JMSAopBundle(),
+      new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+      new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+      new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+      new IDCI\Bundle\ExporterBundle\IDCIExporterBundle(),
+      new fibe\Bundle\WWWConfBundle\fibeWWWConfBundle(),
+      new fibe\SecurityBundle\fibeSecurityBundle(),
+      new fibe\MobileAppBundle\fibeMobileAppBundle(),
+      new fibe\UserDataBundle\fibeUserDataBundle(),
+      new fibe\DashboardBundle\fibeDashboardBundle(),
+      new fibe\HomePageBundle\fibeHomePageBundle(),
+      new fibe\DataBundle\DataBundle(),
+      new fibe\ExternalizationBundle\fibeExternalizationBundle(),
+      new fibe\ConferenceBundle\fibeConferenceBundle(),
+      new fibe\DocumentationBundle\DocumentationBundle(),
+      new FOS\UserBundle\FOSUserBundle(),
+      new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
+      new Lexik\Bundle\FormFilterBundle\LexikFormFilterBundle()
+    );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
-
-        return $bundles;
+    if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+      $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+      $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+      $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
-    }
+    return $bundles;
+  }
+
+  public function registerContainerConfiguration(LoaderInterface $loader)
+  {
+    $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
+  }
 }
