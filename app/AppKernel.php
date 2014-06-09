d663cb35ad83cb0f899f1979d0cad4bf5aadb73e
Vincent Sébille <vincent.sebille@gmail.com>
2014-06-09 19:41:11 +0200

    [code review] refacto app directory

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,7 +36,8 @@ class AppKernel extends Kernel
       new Lexik\Bundle\FormFilterBundle\LexikFormFilterBundle()
     );
 
-    if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+    if (in_array($this->getEnvironment(), array('dev', 'test')))
+    {
       $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
       $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
       $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
