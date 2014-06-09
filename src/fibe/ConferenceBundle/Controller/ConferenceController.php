be989358b16ccf1f18d368d7a02affc00cea9fd9
Vincent Sébille <vincent.sebille@gmail.com>
2014-06-09 19:31:01 +0200

    [code review] refacto ConferenceBundle

--- src/fibe/ConferenceBundle/Controller/ConferenceController.php
+++ src/fibe/ConferenceBundle/Controller/ConferenceController.php
@@ -1,239 +1,240 @@
 <?php
 
-  namespace fibe\ConferenceBundle\Controller;
+namespace fibe\ConferenceBundle\Controller;
 
-  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-  use Symfony\Component\HttpFoundation\Request;
-  use Symfony\Component\HttpFoundation\Response;
-  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-  use fibe\Bundle\WWWConfBundle\Entity\WwwConf;
-  use fibe\Bundle\WWWConfBundle\Entity\ConfEvent;
-  use fibe\MobileAppBundle\Entity\MobileAppConfig;
-  use fibe\Bundle\WWWConfBundle\Form\WwwConfType;
-  use fibe\Bundle\WWWConfBundle\Form\ModuleType; 
+use fibe\Bundle\WWWConfBundle\Entity\WwwConf;
+use fibe\Bundle\WWWConfBundle\Entity\ConfEvent;
+use fibe\MobileAppBundle\Entity\MobileAppConfig;
+use fibe\Bundle\WWWConfBundle\Form\WwwConfType;
+use fibe\Bundle\WWWConfBundle\Form\ModuleType;
 
-  use fibe\SecurityBundle\Entity\Team; 
+use fibe\SecurityBundle\Entity\Team;
 
-  use fibe\Bundle\WWWConfBundle\Form\XPropertyType;
-  use fibe\Bundle\WWWConfBundle\Form\EventType;
-  use fibe\Bundle\WWWConfBundle\Entity\XProperty;
-  use fibe\Bundle\WWWConfBundle\Entity\Event;
-  use fibe\Bundle\WWWConfBundle\Entity\Location;
-  use fibe\Bundle\WWWConfBundle\Entity\Module;
+use fibe\Bundle\WWWConfBundle\Form\XPropertyType;
+use fibe\Bundle\WWWConfBundle\Form\EventType;
+use fibe\Bundle\WWWConfBundle\Entity\XProperty;
+use fibe\Bundle\WWWConfBundle\Entity\Event;
+use fibe\Bundle\WWWConfBundle\Entity\Location;
+use fibe\Bundle\WWWConfBundle\Entity\Module;
 
 
-  use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-  use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+/**
+ * Link controller.
+ *
+ * @Route("/admin/conference")
+ */
+class ConferenceController extends Controller
+{
+
 
   /**
-   * Link controller.
+   * @Route("", name="schedule_conference_show")
    *
-   * @Route("/admin/conference")
+   * @Template()
    */
-  class ConferenceController extends Controller
+  public function showAction(Request $request)
   {
+    $conference = $this->get('fibe_security.acl_entity_helper')->getEntityACL('VIEW', 'WwwConf', $this->getUser()->getCurrentConf());
 
+    $mainConfEvent = $conference->getMainConfEvent();
+    $form = $this->createForm(new WwwConfType($this->getUser()), $conference);
 
-    /**
-     * @Route("", name="schedule_conference_show")
-     *
-     * @Template()
-     */
-    public function showAction(Request $request)
+    $request = $this->get('request');
+    if ($request->getMethod() == 'POST')
     {
-      $conference = $this->get('fibe_security.acl_entity_helper')->getEntityACL('VIEW','WwwConf',$this->getUser()->getCurrentConf());
-      
-      $mainConfEvent = $conference->getMainConfEvent();
-      $form = $this->createForm(new WwwConfType($this->getUser()), $conference);
+      $form->bind($request);
 
-      $request = $this->get('request');
-      if ($request->getMethod() == 'POST')
+      if ($form->isValid())
       {
-        $form->bind($request);
+        $em = $this->getDoctrine()->getManager();
+        $conference->slugify();
+        $em->persist($conference);
+        $conference->uploadLogo();
+        $em->flush();
 
-        if ($form->isValid())
-        {
-          $em = $this->getDoctrine()->getManager();
-          $conference->slugify();
-          $em->persist($conference);
-          $conference->uploadLogo();
-          $em->flush();
-
-          $this->container->get('session')->getFlashBag()->add(
-            'success',
-            'The conference has been successfully updated'
-          );
-        }
-        else
-        {
-
-          $this->container->get('session')->getFlashBag()->add(
-            'error',
-            'Submition error, please try again.'
-          );
-        }
+        $this->container->get('session')->getFlashBag()->add(
+          'success',
+          'The conference has been successfully updated'
+        );
       }
+      else
+      {
 
-      return array(
-        'conference' => $conference,
-        'form'       => $form->createView(), 
-      );
-
-    } 
-
-
-    /**
-     * @Route("/{id}/empty", name="schedule_conference_empty")
-     */
-    public function emptyAction(Request $request, $id)
-    {
-      $em = $this->getDoctrine()->getManager();
-
-      $conference = $this->get('fibe_security.acl_entity_helper')->getEntityACL('DELETE','WwwConf',$this->getUser()->getCurrentConf());
-
-      //TODO CSRF TOKEN
-      // $csrf = $this->get('form.csrf_provider'); //Symfony\Component\Form\Extension\Csrf\CsrfProvider\SessionCsrfProvider 
-      // $token = $csrf->generateCsrfToken($intention); //Intention should be empty string, if you did not define it in parameters
-      // BOOLEAN $csrf->isCsrfTokenValid($intention, $token); 
-
-      $emptyConf = $this->get('emptyConf');
-      $emptyConf->emptyConf($conference, $em);
-
-      $em->flush();
-
-      $this->container->get('session')->getFlashBag()->add(
-        'success',
-        'conference successfully emptied.'
-      );
-      return $this->redirect($this->generateUrl('schedule_conference_show'));
+        $this->container->get('session')->getFlashBag()->add(
+          'error',
+          'Submition error, please try again.'
+        );
+      }
     }
 
-    /**
-     * Creates a new COnference.
-     * @Route("/create", name="schedule_conference_create")
-     */
-    public function createAction(Request $request)
-    {
-      //Persist Conference
-      $em = $this->getDoctrine()->getManager();
+    return array(
+      'conference' => $conference,
+      'form'       => $form->createView(),
+    );
 
-      //Create the default conference
-      $entity = new WwwConf();
-      $entity->setLogoPath("livecon.png");
-      $em->persist($entity);
-
-      //Session user
-      $user = $this->getUser();
+  }
 
 
-      //Module
-      $defaultModule = new Module();
-      $defaultModule->setPaperModule(1);
-      $defaultModule->setOrganizationModule(1);
-      $defaultModule->setSponsorModule(1);
-      $defaultModule->setConference($entity);
-      $em->persist($defaultModule);
+  /**
+   * @Route("/{id}/empty", name="schedule_conference_empty")
+   */
+  public function emptyAction(Request $request, $id)
+  {
+    $em = $this->getDoctrine()->getManager();
 
-      //Create new App config for the conference
-      $defaultAppConfig = new MobileAppConfig();
+    $conference = $this->get('fibe_security.acl_entity_helper')->getEntityACL('DELETE', 'WwwConf', $this->getUser()->getCurrentConf());
 
-      //header color
-      $defaultAppConfig->setBGColorHeader("#f2f2f2");
-      $defaultAppConfig->setTitleColorHeader("#000000");
-      //navBar color
-      $defaultAppConfig->setBGColorNavBar("#305c6b");
-      $defaultAppConfig->setTitleColorNavBar("#f3f6f6");
-      //content color
-      $defaultAppConfig->setBGColorContent("#f3f6f6");
-      $defaultAppConfig->setTitleColorContent("#8c949c");
-      //buttons color
-      $defaultAppConfig->setBGColorButton("#f3f6f6");
-      $defaultAppConfig->setTitleColorButton("#000000");
-      //footer color
-      $defaultAppConfig->setBGColorfooter("#305c6b");
-      $defaultAppConfig->setTitleColorFooter("#f3f6f6");
-      $defaultAppConfig->setIsPublished(true);
-      $defaultAppConfig->setDblpDatasource(true);
-      $defaultAppConfig->setGoogleDatasource(true);
-      $defaultAppConfig->setDuckduckgoDatasource(true);
-      $defaultAppConfig->setLang("EN");
+    //TODO CSRF TOKEN
+    // $csrf = $this->get('form.csrf_provider'); //Symfony\Component\Form\Extension\Csrf\CsrfProvider\SessionCsrfProvider
+    // $token = $csrf->generateCsrfToken($intention); //Intention should be empty string, if you did not define it in parameters
+    // BOOLEAN $csrf->isCsrfTokenValid($intention, $token);
 
-      $em->persist($defaultAppConfig);
+    $emptyConf = $this->get('emptyConf');
+    $emptyConf->emptyConf($conference, $em);
 
-      $categorie = $em->getRepository('fibeWWWConfBundle:Category')->findOneByName("ConferenceEvent");
+    $em->flush();
 
-      //Main conf event
-      $mainConfEvent = new ConfEvent();
-      $mainConfEvent->setSummary("Livecon Conference");
-      $mainConfEvent->setIsMainConfEvent(true);
-      $mainConfEvent->setStartAt(new \DateTime('now'));
-      $end = new \DateTime('now');
-      $mainConfEvent->setEndAt($end->add(new \DateInterval('P2D')));
-      $mainConfEvent->addCategorie($categorie);
-      $mainConfEvent->setConference($entity);
-      $em->persist($mainConfEvent);
+    $this->container->get('session')->getFlashBag()->add(
+      'success',
+      'conference successfully emptied.'
+    );
+
+    return $this->redirect($this->generateUrl('schedule_conference_show'));
+  }
+
+  /**
+   * Creates a new COnference.
+   * @Route("/create", name="schedule_conference_create")
+   */
+  public function createAction(Request $request)
+  {
+    //Persist Conference
+    $em = $this->getDoctrine()->getManager();
+
+    //Create the default conference
+    $entity = new WwwConf();
+    $entity->setLogoPath("livecon.png");
+    $em->persist($entity);
+
+    //Session user
+    $user = $this->getUser();
 
 
-      // conference location
-      $mainConfEventLocation = new Location();
-      $mainConfEventLocation->setName("Conference's location");
-      $mainConfEventLocation->addLocationAwareCalendarEntitie($mainConfEvent);
-      $mainConfEventLocation->setConference($entity);
-      $em->persist($mainConfEventLocation);
-      $mainConfEvent->setLocation($mainConfEventLocation);
-      $em->persist($mainConfEvent);
+    //Module
+    $defaultModule = new Module();
+    $defaultModule->setPaperModule(1);
+    $defaultModule->setOrganizationModule(1);
+    $defaultModule->setSponsorModule(1);
+    $defaultModule->setConference($entity);
+    $em->persist($defaultModule);
 
-      //Team
-      $defaultTeam = new Team();
-      $defaultTeam->addConfManager($user);
-      $user->addTeam($defaultTeam);
-      $defaultTeam->setConference($entity);
-      $entity->setTeam($defaultTeam);
-      $em->persist($defaultTeam); 
+    //Create new App config for the conference
+    $defaultAppConfig = new MobileAppConfig();
 
-      //Linking app config to conference
-      $entity->setAppConfig($defaultAppConfig);
-      $entity->setMainConfEvent($mainConfEvent);
-      $entity->setModule($defaultModule);
+    //header color
+    $defaultAppConfig->setBGColorHeader("#f2f2f2");
+    $defaultAppConfig->setTitleColorHeader("#000000");
+    //navBar color
+    $defaultAppConfig->setBGColorNavBar("#305c6b");
+    $defaultAppConfig->setTitleColorNavBar("#f3f6f6");
+    //content color
+    $defaultAppConfig->setBGColorContent("#f3f6f6");
+    $defaultAppConfig->setTitleColorContent("#8c949c");
+    //buttons color
+    $defaultAppConfig->setBGColorButton("#f3f6f6");
+    $defaultAppConfig->setTitleColorButton("#000000");
+    //footer color
+    $defaultAppConfig->setBGColorfooter("#305c6b");
+    $defaultAppConfig->setTitleColorFooter("#f3f6f6");
+    $defaultAppConfig->setIsPublished(true);
+    $defaultAppConfig->setDblpDatasource(true);
+    $defaultAppConfig->setGoogleDatasource(true);
+    $defaultAppConfig->setDuckduckgoDatasource(true);
+    $defaultAppConfig->setLang("EN");
 
-      //Add conference to current manager
-      $user->setCurrentConf($entity);
-      $user->addConference($entity);
+    $em->persist($defaultAppConfig);
 
-      $em->persist($user);
-      $em->persist($entity);
-      $em->flush();
+    $categorie = $em->getRepository('fibeWWWConfBundle:Category')->findOneByName("ConferenceEvent");
 
-      //Create slug after persist => visibleon endpoint
-      $entity->slugify();
-      $em->persist($entity);
-      $em->flush();
+    //Main conf event
+    $mainConfEvent = new ConfEvent();
+    $mainConfEvent->setSummary("Livecon Conference");
+    $mainConfEvent->setIsMainConfEvent(true);
+    $mainConfEvent->setStartAt(new \DateTime('now'));
+    $end = new \DateTime('now');
+    $mainConfEvent->setEndAt($end->add(new \DateInterval('P2D')));
+    $mainConfEvent->addCategorie($categorie);
+    $mainConfEvent->setConference($entity);
+    $em->persist($mainConfEvent);
 
 
-      return $this->redirect($this->generateUrl('schedule_conference_show'));
-    } 
+    // conference location
+    $mainConfEventLocation = new Location();
+    $mainConfEventLocation->setName("Conference's location");
+    $mainConfEventLocation->addLocationAwareCalendarEntitie($mainConfEvent);
+    $mainConfEventLocation->setConference($entity);
+    $em->persist($mainConfEventLocation);
+    $mainConfEvent->setLocation($mainConfEventLocation);
+    $em->persist($mainConfEvent);
 
-    /*************************** MODULE *************************/
+    //Team
+    $defaultTeam = new Team();
+    $defaultTeam->addConfManager($user);
+    $user->addTeam($defaultTeam);
+    $defaultTeam->setConference($entity);
+    $entity->setTeam($defaultTeam);
+    $em->persist($defaultTeam);
 
-    /**
-     * @Route("/module", name="schedule_conference_module")
-     *
-     * @Template()
-     */
-    public function moduleAction(Request $request)
-    { 
-      $module = $this->get('fibe_security.acl_entity_helper')->getEntityACL('EDIT','Module',$this->getUser()->getCurrentConf()->getModule());
+    //Linking app config to conference
+    $entity->setAppConfig($defaultAppConfig);
+    $entity->setMainConfEvent($mainConfEvent);
+    $entity->setModule($defaultModule);
 
-      $moduleForm = $this->createForm(new ModuleType(), $module); 
+    //Add conference to current manager
+    $user->setCurrentConf($entity);
+    $user->addConference($entity);
 
-      return array( 
-        'module'      => $module,
-        'module_form' => $moduleForm->createView(),
-      );
+    $em->persist($user);
+    $em->persist($entity);
+    $em->flush();
 
-    }
+    //Create slug after persist => visibleon endpoint
+    $entity->slugify();
+    $em->persist($entity);
+    $em->flush();
+
+
+    return $this->redirect($this->generateUrl('schedule_conference_show'));
+  }
+
+  /*************************** MODULE *************************/
+
+  /**
+   * @Route("/module", name="schedule_conference_module")
+   *
+   * @Template()
+   */
+  public function moduleAction(Request $request)
+  {
+    $module = $this->get('fibe_security.acl_entity_helper')->getEntityACL('EDIT', 'Module', $this->getUser()->getCurrentConf()->getModule());
+
+    $moduleForm = $this->createForm(new ModuleType(), $module);
+
+    return array(
+      'module'      => $module,
+      'module_form' => $moduleForm->createView(),
+    );
+
+  }
 
 
   /**
@@ -249,8 +250,8 @@
   public function updateModuleAction(Request $request, $id)
   {
 
-    $entity = $this->get('fibe_security.acl_entity_helper')->getEntityACL('EDIT','Module',$this->getUser()->getCurrentConf()->getModule());
- 
+    $entity = $this->get('fibe_security.acl_entity_helper')->getEntityACL('EDIT', 'Module', $this->getUser()->getCurrentConf()->getModule());
+
     $editForm = $this->createForm(new ModuleType(), $entity);
     $editForm->bind($request);
 
@@ -278,32 +279,33 @@
   }
 
 
-    /**
-     * @Route("/{id}/delete", name="schedule_conference_delete")
-     */
-    public function deleteAction(Request $request, $id)
-    {
-      $em = $this->getDoctrine()->getManager();
-      $user = $this->getUser();
+  /**
+   * @Route("/{id}/delete", name="schedule_conference_delete")
+   */
+  public function deleteAction(Request $request, $id)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $user = $this->getUser();
 
-      $conference = $this->get('fibe_security.acl_entity_helper')->getEntityACL('DELETE','WwwConf',$id);
+    $conference = $this->get('fibe_security.acl_entity_helper')->getEntityACL('DELETE', 'WwwConf', $id);
 
-      //Change User current Conf
-      $user->setCurrentConf(null);
-      $em->persist($user);
+    //Change User current Conf
+    $user->setCurrentConf(null);
+    $em->persist($user);
 
-      //Empty conf datas
-      $emptyConf = $this->get('emptyConf');
-      $emptyConf->prepareDeleteConf($conference, $em);
-      $em->remove($conference);
-      $em->flush();
+    //Empty conf datas
+    $emptyConf = $this->get('emptyConf');
+    $emptyConf->prepareDeleteConf($conference, $em);
+    $em->remove($conference);
+    $em->flush();
 
-      $this->container->get('session')->getFlashBag()->add(
-        'success',
-        'conference successfully deleted.'
-      );
-      return $this->redirect($this->generateUrl('dashboard_index'));
-    }
+    $this->container->get('session')->getFlashBag()->add(
+      'success',
+      'conference successfully deleted.'
+    );
 
-
+    return $this->redirect($this->generateUrl('dashboard_index'));
   }
+
+
+}
--- src/fibe/ConferenceBundle/Form/UserAuthorizationType.php
+++ src/fibe/ConferenceBundle/Form/UserAuthorizationType.php
@@ -1,108 +1,108 @@
 <?php
 
-  namespace fibe\ConferenceBundle\Form;
+namespace fibe\ConferenceBundle\Form;
 
-  use Symfony\Component\Form\AbstractType;
-  use Symfony\Component\Form\FormBuilderInterface;
-  use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-  use fibe\SecurityBundle\Form\AuthorizationType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use fibe\SecurityBundle\Form\AuthorizationType;
+
+/**
+ * @TODO comment
+ *
+ */
+class UserAuthorizationType extends AbstractType
+{
+
+  private $user;
 
   /**
-   * @TODO comment
-   * 
+   * @param $user
    */
-  class UserAuthorizationType extends AbstractType
+  public function __construct($user)
   {
+    $this->user = $user;
+  }
 
-    private $user;
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+      ->remove('user')
+      ->add('flagApp', 'checkbox', array(
+          'required' => false,
+          'label'    => 'Mobile Application Manager',
+          'attr'     => array('class'          => 'switch switch-small',
+                              'data-on-label'  => "<i class='fa fa-check fa-white'>",
+                              'data-off-label' => "<i class='fa fa-ban'>",
+          ))
+      )
+      ->add('flagSched', 'checkbox', array(
+          'required' => false,
+          'label'    => 'Schedule Manager',
+          'attr'     => array('class'          => 'switch switch-small',
+                              'data-on-label'  => "<i class='fa fa-check fa-white'>",
+                              'data-off-label' => "<i class='fa fa-ban'>",
+          ))
 
-    /**
-     * @param $user
-     */
-    public function __construct($user)
-    {
-      $this->user = $user;
-    }
+      )
+      ->add('flagconfDatas', 'checkbox', array(
+          'required' => false,
+          'label'    => 'Datas Conference Manager',
+          'attr'     => array('class'          => 'switch switch-small',
+                              'data-on-label'  => "<i class='fa fa-check fa-white'>",
+                              'data-off-label' => "<i class='fa fa-ban'>",
+          ))
+      );
 
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    if (count($this->user->getCurrentConf()->getConfManagers()) <= 1)
     {
       $builder
-        ->remove('user')
-        ->add('flagApp', 'checkbox', array(
+        ->add('flagTeam', 'checkbox', array(
             'required' => false,
-            'label'    => 'Mobile Application Manager',
-            'attr'     => array('class'          => 'switch switch-small',
-                                'data-on-label'  => "<i class='fa fa-check fa-white'>",
-                                'data-off-label' => "<i class='fa fa-ban'>",
-            ))
-        )
-        ->add('flagSched', 'checkbox', array(
-            'required' => false,
-            'label'    => 'Schedule Manager',
-            'attr'     => array('class'          => 'switch switch-small',
-                                'data-on-label'  => "<i class='fa fa-check fa-white'>",
-                                'data-off-label' => "<i class='fa fa-ban'>",
-            ))
-
-        )
-        ->add('flagconfDatas', 'checkbox', array(
-            'required' => false,
-            'label'    => 'Datas Conference Manager',
+            'disabled' => true,
+            'label'    => 'Team Manager',
             'attr'     => array('class'          => 'switch switch-small',
                                 'data-on-label'  => "<i class='fa fa-check fa-white'>",
                                 'data-off-label' => "<i class='fa fa-ban'>",
             ))
         );
 
-      if (count($this->user->getCurrentConf()->getConfManagers()) <= 1)
-      {
-        $builder
-          ->add('flagTeam', 'checkbox', array(
-              'required' => false,
-              'disabled' => true,
-              'label'    => 'Team Manager',
-              'attr'     => array('class'          => 'switch switch-small',
-                                  'data-on-label'  => "<i class='fa fa-check fa-white'>",
-                                  'data-off-label' => "<i class='fa fa-ban'>",
-              ))
-          );
-
-      }
-      else
-      {
-
-        $builder
-          ->add('flagTeam', 'checkbox', array(
-              'required' => false,
-              'label'    => 'Team Manager',
-              'attr'     => array('class'          => 'switch switch-small',
-                                  'data-on-label'  => "<i class='fa fa-check fa-white'>",
-                                  'data-off-label' => "<i class='fa fa-ban'>",
-              ))
-          );
-      }
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    else
     {
-      $resolver->setDefaults(array(
-        'data_class'   => 'fibe\SecurityBundle\Entity\Authorization'
-      ));
-    }
 
-    /**
-     * Returns the name of this type.
-     *
-     * @return string The name of this type
-     */
-    public function getName()
-    {
-      return 'fibe_securitybundle_authorizationtype';
+      $builder
+        ->add('flagTeam', 'checkbox', array(
+            'required' => false,
+            'label'    => 'Team Manager',
+            'attr'     => array('class'          => 'switch switch-small',
+                                'data-on-label'  => "<i class='fa fa-check fa-white'>",
+                                'data-off-label' => "<i class='fa fa-ban'>",
+            ))
+        );
     }
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setDefaultOptions(OptionsResolverInterface $resolver)
+  {
+    $resolver->setDefaults(array(
+      'data_class' => 'fibe\SecurityBundle\Entity\Authorization'
+    ));
+  }
+
+  /**
+   * Returns the name of this type.
+   *
+   * @return string The name of this type
+   */
+  public function getName()
+  {
+    return 'fibe_securitybundle_authorizationtype';
+  }
+}
